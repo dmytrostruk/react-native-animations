053338a575329dd7915d749cf11b5a6e44e7f856
Dmytro Struk <dmytro.struk1@gmail.com>
2021-12-24 18:55:21 +0200

    Added temporary photos

--- components/ParallaxCarousel.tsx
+++ components/ParallaxCarousel.tsx
@@ -15,7 +15,13 @@ const ITEM_WIDTH = width * 0.76;
 const ITEM_HEIGHT = ITEM_WIDTH * 1.47;
 
 // Insert some images
-const images = [""];
+const images = [
+  "https://images.unsplash.com/photo-1534270804882-6b5048b1c1fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+  "https://images.unsplash.com/photo-1490644658840-3f2e3f8c5625?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+  "https://images.unsplash.com/photo-1522092372459-dff01028d904?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+  "https://images.unsplash.com/photo-1476837754190-8036496cea40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
+  "https://images.unsplash.com/photo-1518218392262-1beb4730fb09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
+];
 
 const data = images.map((image, index) => ({
   key: String(index),
@@ -60,7 +66,7 @@ export default function ParallaxCarousel() {
                       width: ITEM_WIDTH * 1.4,
                       height: ITEM_HEIGHT,
                       resizeMode: "cover",
-                      transform: [{translateX}]
+                      transform: [{ translateX }],
                     }}
                     source={{ uri: item.photo }}
                   />
@@ -98,5 +104,5 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     alignItems: "center",
     borderRadius: 14,
-  }
+  },
 });
